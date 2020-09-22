1a712e46ff4637099955db293aeff620bdd6b73e
saint-lag <maiasena.gabriel@gmail.com>
2020-09-22 19:43:34 -0300

    update

--- TAC2/exercicio_numero_1.py
+++ TAC2/exercicio_numero_1.py
@@ -1,7 +1,31 @@
-#import numpy as np
-#import pandas as pd 
+
+import numpy as np
+import pandas as pd 
 
 
 #WHOTBDATA = pd.read_excel("Users/maias/Documents/GitHub/CFB017-SAINT/TAC2/WHOPOPTB.xls")
 
-print("WHOTBDATA")
+with open(r"C:\Users\maias\Documents\GitHub\CFB017-SAINT\TAC2\WHOPOPTB.xls") as WHOPOPTB:
+
+	WHOTBDATA = pd.read_excel("WHOPOPTB.xls")
+
+	TB_deaths = WHOTBDATA["TB deaths"]
+	TB_total_deaths = TB_deaths.sum()
+	TB_min_deaths = TB_deaths.min()
+	TB_max_deaths = TB_deaths.max()
+	TB_mean_deaths = TB_deaths.mean()
+	TB_median_deaths = TB_deaths.median()
+
+	print("Exercícios 1 até 5:\n\n",WHOTBDATA)
+	print("\nTB_total_deaths: ",TB_total_deaths)
+	print("\nTB_min_deaths: ",TB_min_deaths)
+	print("\nTB_max_deaths: ",TB_max_deaths)
+	print("\nTB_mean_deaths: ",TB_mean_deaths)
+	print("\nTB_median_deaths: ",TB_median_deaths)
+
+	print("\nExercícios 5 até 7:\n")
+
+	BRICS_TB_total_deaths = ""
+
+
+	WHOPOPTB.close()
